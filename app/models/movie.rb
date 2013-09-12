b5597311fd31bec887496abcbd000c1a25a63f9d
lanstartupstanford <lan.startup.stanford@gmail.com>
2013-09-12 21:33:58 +0700

    fix bug filtering

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
   def self.filter_by_ratings(ratings)
     result = []
     ratings.each do |r|
-      result << Movie.find_by_rating(r)
+      result += Movie.find_all_by_rating(r)
     end
     return result
   end
